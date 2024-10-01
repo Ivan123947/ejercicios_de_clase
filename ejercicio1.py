38994dfe66f61ed13daaeb0a2273c0db3c579d03
Ivan123947 <reyes.vargas.ivan@iescamas.es>
2024-10-01 10:49:07 +0200

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+#esto es un programa que convierte euros en dolares
+print("dime una cantidad")
+num1 = int(input())
+num2 = float(1.10)
+#realizamos la operación
+total = num1*num2
+print("el resultado de sumar",num1,"*",num2,"=",total)
