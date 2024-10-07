121ab19343c6fe48b5c033f18c82749387c7d8de
ivan reyes <reyes.vargas.ivan@iescamas.es>
2024-10-07 10:04:31 +0200

    Add files via upload

---
+++ ejercicio.7.py
@@ -0,0 +1,9 @@
+#este programa se encarga de calcular la media de las 3 notas
+print("dime la nota 1")
+nota1 = float(input())
+print("dime la nota2")
+nota2 = float(input())
+print("dime la nota3")
+nota3 = float(input())
+media = (nota1+nota2+nota3)/3
+print("el resultado de la nota es",media)
