79f2fad41be5338cb6090286bc06e8af87e4e495
ivan reyes <reyes.vargas.ivan@iescamas.es>
2024-10-14 09:50:49 +0200

    Add files via upload

---
+++ secuenciales/ejercicio.4.py
@@ -0,0 +1,4 @@
+#Escribe un programa que convierta una longitud en metros a kilómetros, centímetros o milímetros.
+print("dime una longitud en metros")
+kilometros = float(input())
+
---
+++ secuenciales/ejercicio3.py
@@ -0,0 +1,10 @@
+# escribe un programa que calcula el Índice de Masa Corporal de una persona pidiendole su peso en kilogramos y su altura en metros
+print("dime tu peso") 
+peso = float (input())
+print(type(peso))
+#print(type(peso))
+
+print("dime tu altura")
+altura = float(input())
+print(type(altura))
+#print(type(altura))
