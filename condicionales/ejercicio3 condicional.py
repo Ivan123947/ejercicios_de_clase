b48cd6bae36ba01facb4be8f22c81f6639ec2167
ivan reyes <reyes.vargas.ivan@iescamas.es>
2024-10-14 10:03:33 +0200

    Add files via upload

---
+++ condicionales/ejercicio3 condicional.py
@@ -0,0 +1,13 @@
+#Escribe un programa que le pida al usuario ingresar un nombre de usuario y una contraseña. 
+# Si el nombre de usuario es "admin" y la contraseña es "1234", el programa debe mostrar "Acceso concedido". Si no, debe mostrar "Acceso denegado". 
+# Este tipo de programa se utiliza en muchos sitios web y sistemas para controlar el acceso. 
+USUARIO_BBDD = "admin"
+PASSWORD_BBDD = 1234
+print("escribe un nombre para tu usuario")
+usuario = input()
+print("es necesario que te inventes una contraseña")
+contraseña = (input())
+if(usuario == USUARIO_BBDD) and (contraseña == PASSWORD_BBDD): 
+    print("acceso concedido")
+else: 
+    print("acceso denegado")
