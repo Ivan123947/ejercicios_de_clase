9fad7a280b19f2018246c8ec85f1531acc7d650c
ivan reyes <reyes.vargas.ivan@iescamas.es>
2024-10-22 11:12:50 +0200

    Add files via upload

---
+++ bucle/ejercicio 2 bucle.py
@@ -0,0 +1,5 @@
+#Escribe un programa que calcule la suma de todos los números del 1 al 100 utilizando un bucle 
+acumulador = 0 
+for numero in range(1,101):
+    acumulador = acumulador + numero
+print("el total acumulado es", acumulador)
