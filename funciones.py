d158a3f2b33807cb9ae5feaddf81f539b3844446
ivan reyes <reyes.vargas.ivan@iescamas.es>
2024-11-04 10:13:35 +0100

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,40 @@
+#definimos funciones
+def resta(a,b):
+    resultado = a - b
+    return resultado
+print(resta(8,2))
+
+def multiplicación (k, z):
+    resultado = k * z
+    return resultado
+print(multiplicación(8,5))
+
+def siguiente (num):
+    resultado = num + 1
+    return resultado
+print(siguiente(10))
+
+def mayor_de_dos (x, y):
+    if (x > y):
+        return x
+    else:
+        return y
+print(mayor_de_dos(4,8))
+
+def mayor_de_tres(x, y, z):
+    if (x > y):
+        if (x > z):
+            return x
+        else:
+            return z
+    else:
+        if (y > z):
+            return y
+        else:
+            return z
+print (mayor_de_tres(5, 10, 13))
+
+def mayor_de_cuatro (v1, v2, v3, v4):
+    return mayor_de_tres(mayor_de_dos(v1,v2), v3, v4)
+print(mayor_de_cuatro(3, 4, 5, 6))
+    
