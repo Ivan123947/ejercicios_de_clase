9927129c9cb3ff3cc931743f2e985deaf4668dd1
ivan reyes <reyes.vargas.ivan@iescamas.es>
2024-11-05 11:08:08 +0100

    Add files via upload

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -37,4 +37,19 @@ print (mayor_de_tres(5, 10, 13))
 def mayor_de_cuatro (v1, v2, v3, v4):
     return mayor_de_tres(mayor_de_dos(v1,v2), v3, v4)
 print(mayor_de_cuatro(3, 4, 5, 6))
-    
+
+def contar_vocales(texto):
+    contador = 0
+    for caracter in texto:
+        if (caracter == 'o'):
+            contador = contador + 1
+        elif(caracter == 'e'):
+            contador = contador + 1
+        elif(caracter == 'i'):
+            contador = contador + 1
+        elif(caracter == 'a'):
+            contador = contador + 1
+        elif(caracter == 'u'):
+           contador = contador + 1
+    return contador
+print(contar_vocales("hola mundo"))
